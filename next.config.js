e222cb2a957959ea37050d61a5ecff27482f77fc
Wojciech Mierzwa <wojciech.piotr.mierzwa@gmail.com>
2024-12-19 15:36:43 +0100

    Test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', 
-    images: {
-      unoptimized: true, 
-    },
+module.exports = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  productionBrowserSourceMaps: false,
 };
-
-module.exports = nextConfig;
